bd19f8b537e3189fa8c355a684bf9bf1bc9b60aa
DylonFleming11 <Dylonfleming11@yahoo.com>
2021-05-13 11:25:15 -0500

    First stopping point modal added, sign up/in/out added

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,1 +1,36 @@
-asdfasdfasdf
+'use strict'
+
+const getFormFields = require('./../../lib/get-form-fields')
+const api = require('./api')
+const ui = require('./ui')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log(data, 'The form data! Sign Up')
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  console.log(data, 'The form data! Sign In')
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+  api.signOut()
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
+module.exports = {
+  onSignIn,
+  onSignUp,
+  onSignOut
+}
--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,1 +1,47 @@
-asdfasdfasdf
+'use strict'
+const store = require('./store')
+
+const signUpSuccess = function (res) {
+  $('#sign-up').trigger('reset')
+  $('#messaging').text('Sign up succesfully!')
+}
+
+const signUpFailure = function (err) {
+  console.log(err)
+  $('#messaging').text('Sign in error' + err)
+}
+
+const signInSuccess = function (res) {
+  $('#sign-in').trigger('reset')
+  $('#messaging').text('Sign in succesfully!')
+  console.log(store)
+  store.user = res.user
+  console.log(store)
+  $('#after-sign-in').show()
+  $('#before-sign-in').hide()
+}
+
+const signInFailure = function (err) {
+  console.log(err)
+  $('#messaging').text('Sign in error' + err)
+}
+
+const signOutSuccess = function (res) {
+  store.user = null
+  $('#messaging').text('succesfully Signed Out!')
+  $('#after-sign-in').hide()
+  $('#before-sign-in').show()
+}
+
+const signOutFailure = function () {
+  $('#messaging').text('Failed to sign out')
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure
+}
